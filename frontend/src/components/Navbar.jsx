236d27943f64bac3add0dfde21cd8e939e15a3dd
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-22 18:09:02 +0530

    for mobile completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" bg-black  flex flex-row justify-around items-center">
-    
-    
-<div className="text-white p-2  flex flex-col"><span className='text-3xl font-bold'>Me & Guys</span><span className='  text-xs tracking-widest font-normal'>U N I S E X  S A L O O N</span></div>
+    <div className="flex flex-row justify-between px-8 bg-black   md:justify-around md:px-0 items-center">
 
-<div className="flex flex-col gap-2">
-    <div class='text-yellow-600 text-end border-1 border-yellow-600'>Book Appointment</div>
-    <div className='flex flex-row gap-5 text-white'>
-        <p>HOME</p>
-        <p>SERVICES</p>
-        <p>E-SHOP</p>
-        <p>OFFER</p>
-        <p>ABOUT</p>
-        <p>CONTACT</p>
+    {/* logo */}
+    <div className="text-white py-4  flex flex-col"><span className='text-3xl font-bold'>Me & Guys</span><span className='  text-xs tracking-widest font-normal'>U N I S E X  S A L O O N</span></div>
+
+{/* options */}
+<div className="hidden md:flex flex-col gap-2">
+    <div className='flex justify-end'>
+        <div className='px-2 text-yellow-600 border-1 border-yellow-600 cursor-pointer hover:text-white'>Book Appointment</div>
+    </div>
+    <div className='flex flex-row gap-5 text-white cursor-pointer'>
+        <p className="hover:text-yellow-600">HOME</p>
+        <p className="hover:text-yellow-600">SERVICES</p>
+        <p className="hover:text-yellow-600">E-SHOP</p>
+        <p className="hover:text-yellow-600">OFFER</p>
+        <p className="hover:text-yellow-600">ABOUT</p>
+        <p className="hover:text-yellow-600">CONTACT</p>
     </div>
 </div>
-
-<div className='text-white'>profile<img></img></div>
+<div className='text-white cursor-pointer'>Signup/login</div>
 
     </div>
   )
