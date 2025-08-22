4c38ee3369f88811811454942b441ae58bb769aa
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-22 16:40:04 +0530

    creating navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="">
+    <div className=" bg-black  flex flex-row justify-around items-center">
     
-<div className="">Me & Guys</div>
-<div className=""></div>
+    
+<div className="text-white p-2 gap-2 flex flex-col"><span>Me & Guys</span><span>SALOON</span></div>
+
+<div className="flex flex-col gap-2">
+    <div class='text-yellow-600 text-end border-1 border-yellow-600'>Book Appointment</div>
+    <div className='flex flex-row gap-5 text-white'>
+        <p>HOME</p>
+        <p>SERVICES</p>
+        <p>E-SHOP</p>
+        <p>OFFER</p>
+        <p>ABOUT</p>
+        <p>CONTACT</p>
+    </div>
+</div>
+
+<div className='text-white'>profile</div>
 
     </div>
   )
