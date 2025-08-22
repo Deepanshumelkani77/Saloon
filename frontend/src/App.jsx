419cbee9f84287a74e9dd206ac0ddd1e5d24fe54
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-22 16:02:36 +0530

    inital commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+ 
+
+  return (
+    <div>
+
+<Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+   
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="">
+    
+<div className="">Me & Guys</div>
+<div className=""></div>
+
+    </div>
+  )
+}
+
+export default Navbar
