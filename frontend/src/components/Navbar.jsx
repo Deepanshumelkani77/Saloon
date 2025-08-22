3f8a92afb5de5eeb20ccf23b9f4994a73c3080a8
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-22 17:45:06 +0530

    logo completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className=" bg-black  flex flex-row justify-around items-center">
     
     
-<div className="text-white p-2 gap-2 flex flex-col"><span>Me & Guys</span><span>SALOON</span></div>
+<div className="text-white p-2  flex flex-col"><span className='text-3xl font-bold'>Me & Guys</span><span className='  text-xs tracking-widest font-normal'>U N I S E X  S A L O O N</span></div>
 
 <div className="flex flex-col gap-2">
     <div class='text-yellow-600 text-end border-1 border-yellow-600'>Book Appointment</div>
@@ -19,7 +19,7 @@ const Navbar = () => {
     </div>
 </div>
 
-<div className='text-white'>profile</div>
+<div className='text-white'>profile<img></img></div>
 
     </div>
   )
