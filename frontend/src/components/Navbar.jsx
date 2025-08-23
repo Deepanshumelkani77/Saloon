aa22953320339fccf34bf277717220caf1718ad5
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-23 13:47:28 +0530

    try to make sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,7 +21,40 @@ const Navbar = () => {
         <p className="hover:text-yellow-600">CONTACT</p>
     </div>
 </div>
-<div className='text-white cursor-pointer'>Signup/login</div>
+
+
+{/* login/signup */}
+<div>
+<div className='hidden md:flex text-white cursor-pointer'>Signup/login</div>
+<button className=" text-white cursor-pointer md:hidden ">menubar</button>
+</div>
+
+
+{/*  sidebar  */}
+<div className="fixed top-0 left-0 w-3/4 h-full bg-black text-white z-50">
+
+<div>
+  <div className=" mt-5 text-white flex flex-col gap-2 items-center"><span className="text-3xl font-bold ">Me & Guys</span><span className="text-xs tracking-widest font-normal">UNISEX SALOON</span></div>
+<button>X</button>
+</div>
+
+
+<div className="mt-5 text-white flex flex-col gap-2 items-center justify-between">
+  <p>HOME</p>
+  <p>SERVICES</p>
+  <p>E-SHOP</p>
+  <p>OFFER</p>
+  <p>ABOUT</p>
+  <p>CONTACT</p>
+</div>
+
+<div className="text-center py-2 mt-5 text-yellow-600 border border-1 border-yellow-600 ">Book Appointment</div>
+
+<div className="flex justify-center"><button className="mt-5  texxt-white">signup/Login</button></div>
+
+</div>
+
+
 
     </div>
   )
