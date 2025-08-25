d8c2e021282de3ba65bac5f3903069657e35a5f1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-25 15:12:13 +0530

    video slider component created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header.jsx';
 import Service from '../components/Service.jsx';
+import VideoSlider from '../components/VideoSlider.jsx';
 
 const Home = () => {
   return (
     <div className=" w-full">
       <Header/>
       <Service/>
+      <VideoSlider/>
     </div>
   )
 }
