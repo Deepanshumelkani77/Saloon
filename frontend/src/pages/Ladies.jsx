46862e5d7d3dc8ab39135451403b9f754e842cc4
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-27 23:53:37 +0530

    one section for ladies completed

--- frontend/src/pages/Ladies.jsx
+++ frontend/src/pages/Ladies.jsx
@@ -2,7 +2,63 @@ import React from 'react'
 
 const Ladies = () => {
   return (
-    <div>
+    <div className="w-full">
+
+<div className="w-full h-[20vh] flex justify-center items-center"><h1 >Services for Ladies</h1></div>
+
+<div className="w-full "><img src="" alt="" /></div>
+
+<div className="w-full flex flex-col md:flex-row justify-around  gap-10 ">
+
+<div className="flex flex-col ">
+    <p>Hair Styling</p>
+   
+        <li>Hair Cut</li>
+        <li>Ironing</li>
+        <li>Global Colouring</li>
+        <li>Blow Dry</li>
+        <li>Root Touch Up</li>
+        <li>Shampoo & Conditioning</li>
+        <li>Head Massage</li>
+        <li>Roller Setting</li>
+        <li>Oiling</li>
+  
+</div>
+<div className="flex flex-col ">
+    <p>Make Up</p>
+    <li>Party Make Up</li>
+    <li>Engagement Make Up</li>
+    <li>Bridal & Reception Make Up</li>
+    <li>Base Make Up</li>
+    <li>Eye Make Up</li>
+</div>
+<div className="flex flex-col ">
+    <p>Hair Treatments</p>
+    <li>Volumizing</li>
+    <li>Spa Treatments</li>
+    <li>Advanced Hair Moisturising</li>
+    <li>Scalp Treatments</li>
+</div>
+<div className="flex flex-col ">
+    <p>Hair Texture</p>
+    <li>Rebonding</li>
+    <li>Perming</li>
+    <li>Keratin</li>
+    <li>Colour Protection</li>
+    <li>Smoothening</li>
+</div>
+
+</div>
+
+<div><img src="" alt="" /></div>
+
+<div>
+
+<div></div>
+<div></div>
+<div></div>
+
+</div>
       
     </div>
   )
