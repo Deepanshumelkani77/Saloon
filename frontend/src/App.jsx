87d8b7c927b64547098c30b999bd7c6d8847688c
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-27 15:09:26 +0530

    try to make services

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import Services from './pages/Services.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className="flex-1 w-full  mx-auto  mt-20">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
       </main>
       <Footer/>
