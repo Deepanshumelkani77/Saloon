d9b638e398b99d13c4cd12772c83e6e3d4e722c7
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-08-30 16:31:58 +0530

    database connected successfully

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,24 @@ const port=1000;
 app.listen(port,()=>{
     console.log("server is running",port);
 })
+
+
+
+
+//database connection
+const mongoose = require("mongoose");
+ const connectDB = async () => {
+    try {
+      await mongoose.connect(
+"mongodb+srv://deepumelkani123_db_user:bhumi77@cluster0.ghgjitk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      );
+      console.log("database connected successfully");
+    } catch (error) {
+      console.error("Error connecting to database:", error);
+    }
+  };
+//db connectin model
+connectDB();
+
+//deepumelkani123_db_user
+//bhumi77
