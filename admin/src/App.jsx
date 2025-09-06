68f20f8af015e46d3b20bb6afa1d28352adbf712
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-06 22:45:24 +0530

    set routes for pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,20 +3,29 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
+import Dashboard from './pages/Dashboard'
+import Appointment from './pages/Appointment'
+import Service from './pages/Service'
+import Specialist from './pages/Specialist'
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Sidebar />
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/appointments' element={<Appointment />} />
+          <Route path='/services' element={<Service />} />
+           <Route path='/specialists' element={<Specialist />} />
+
+        </Routes>
+      </main>
+
       <Footer />
-
-      <Routes>
-
-
-
-      </Routes>
-<Footer/>
 
     </div>
   )
