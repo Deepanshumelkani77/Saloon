edc2a2ec3ccc4a350d0db1ce7fcd93cf4f736fde
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-06 22:39:20 +0530

    created file structure

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Sidebar />
+      <Footer />
+
+      <Routes>
+
+
+
+      </Routes>
+<Footer/>
+
     </div>
   )
 }
 
 export default App
+
+
+      
+  
