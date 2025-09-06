0454823e3d2d2a48ed4c2fff3cdc00810b64b726
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-06 21:50:34 +0530

    added some images

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,21 @@
+import a from './a.png';
+import about from './about.png';
+import b from './b.png';
+import c from './c.png';
+import d from './d.png';
+import gents from './gents.png';
+import img5 from './img5.png';
+import img9 from './img9.png';
+import ladies from './ladies.png';
+
+export default {
+a,
+about,
+b,
+c,
+d,
+gents,
+img5,
+img9,
+ladies
+};
