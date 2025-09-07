9f6710a2be5b6842ff6bbb0cf94be9b200d6dcfa
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-07 18:36:45 +0530

    main set successfully

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
@@ -7,32 +7,43 @@ import Dashboard from './pages/Dashboard'
 import Appointment from './pages/Appointment'
 import Service from './pages/Service'
 import Specialist from './pages/Specialist'
-
+import { AppContext } from './context/AppContext'
 
 const App = () => {
+  const { sidebarOpen } = useContext(AppContext)
+
   return (
-    <div className="">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900">
+      {/* Fixed Navbar */}
       <Navbar />
-      <Sidebar />
 
-      <main>
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/appointments' element={<Appointment />} />
-          <Route path='/services' element={<Service />} />
-           <Route path='/specialists' element={<Specialist />} />
+      {/* Main Layout */}
+      <div className="flex relative">
+        {/* Sidebar */}
+        <Sidebar />
+        
+        {/* Main Content */}
+        <main className={`
+          flex-1 
+          transition-all duration-300 ease-in-out
+          md:ml-64
+          min-h-[calc(100vh-10vh)]
+          p-4 md:p-6 lg:p-8
+          bg-gradient-to-br from-gray-900/30 via-black/30 to-gray-900/30
+        `}>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/appointments' element={<Appointment />} />
+            <Route path='/services' element={<Service />} />
+            <Route path='/specialists' element={<Specialist />} />
+          </Routes>
+        </main>
+      </div>
 
-        </Routes>
-      </main>
-
+      {/* Footer */}
       <Footer />
-
     </div>
   )
 }
 
 export default App
-
-
-      
-  
