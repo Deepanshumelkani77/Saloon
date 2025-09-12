5183556fb56932453c3b2ad9fb2c11ca868a2a59
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-13 00:39:54 +0530

    pages created

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar.jsx"
+import Home from "./pages/Home.jsx"
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
+
+      <main>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+</Routes>
+      </main>
       
     </div>
   )
