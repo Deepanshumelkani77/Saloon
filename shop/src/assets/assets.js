a25633b1fb6ab4d8c3937020e68cc7a7c292c15f
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-13 15:02:58 +0530

    add photos

---
+++ shop/src/assets/assets.js
@@ -0,0 +1,8 @@
+import a1 from "./a1.png"
+import a2 from "./a2.png"
+import a3 from "./a3.png"
+import a4 from "./a4.png"
+
+export const assets={
+    a1,a2,a3,a4
+}
---
+++ shop/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {assets} from "../assets/assets.js"
+
+const Header = () => {
+  return (
+    <div>
+      <img src={assets.a1} alt="" />
+       <img src={assets.a2} alt="" />
+        <img src={assets.a3} alt="" />
+         <img src={assets.a4} alt="" />
+    </div>
+  )
+}
+
+export default Header
