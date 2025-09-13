1ede3c8fd70738ff2dad13858deab022719a320e
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-13 15:22:01 +0530

    created category.jsx

--- shop/src/pages/Home.jsx
+++ shop/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "../components/Header.jsx"
+import Category from "../components/Category.jsx"
 
 const Home = () => {
   return (
     <div>
       
 <Header/>
+<Category/>
 
     </div>
   )
