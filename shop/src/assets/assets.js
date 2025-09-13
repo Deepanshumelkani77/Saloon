a00002bfa6c5f47c697fedc050e68a8bf0029557
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-13 15:50:22 +0530

    added more images

--- shop/src/assets/assets.js
+++ shop/src/assets/assets.js
@@ -2,7 +2,12 @@ import a1 from "./a1.png"
 import a2 from "./a2.png"
 import a3 from "./a3.png"
 import a4 from "./a4.png"
+import hair from "./hair.jpeg"
+import skin from "./skin.jpeg"
+import men from "./men.jpeg"
+import women from "./women.jpeg"
+import accessories from "./dryer.jpeg"
 
 export const assets={
-    a1,a2,a3,a4
+    a1,a2,a3,a4,hair,skin,men,women,accessories
 }
---
+++ shop/src/components/Category.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div className="flex flex-col">
+      
+<div>Shop 100% authentic products directly from your most loved salon chain Looks Salon</div>
+
+<div classNAme="flex flex-row">
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+</div>
+
+    </div>
+  )
+}
+
+export default Category
