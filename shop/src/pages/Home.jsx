843e4047028cc2d50f4d5b0964b5be7942f64658
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-14 14:40:30 +0530

    created bestSeller components

--- shop/src/pages/Home.jsx
+++ shop/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from "../components/Header.jsx"
 import Category from "../components/Category.jsx"
+import BestSeller from "../components/BestSeller.jsx"
 
 const Home = () => {
   return (
     <div className="bg-gradient-to-br from-black via-[#23211b] to-[#181818]">
       <Header/>
       <Category/>
+      <BestSeller/>
     </div>
   )
 }
