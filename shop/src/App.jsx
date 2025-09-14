4152f49169ea8d55fe8d694b1285aef88aa8b589
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-14 18:58:51 +0530

    git push origin main

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar.jsx"
 import Home from "./pages/Home.jsx"
 import Footer from "./components/Footer.jsx"
+import Hair from "./pages/Hair.jsx"
 
 const App = () => {
   return (
