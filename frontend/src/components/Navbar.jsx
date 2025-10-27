67cefc33475e7927ce03a0db9f6bf68941bc3c0d
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-27 23:06:28 +0530

    set all

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import {useContext} from 'react'
 const navLinks = [
   { name: 'HOME', path: '/' },
   { name: 'SERVICES' },
-  { name: 'E-SHOP', path: 'https://saloon-shop-s0r5.onrender.com', external: true },
+  { name: 'E-SHOP', path: 'https://saloon-shopp.vercel.app', external: true },
   { name: 'OFFER', path: '/offer' },
   { name: 'ABOUT', path: '/about' },
   { name: 'CONTACT', path: '/contact' },
